ccb1ab50b1e6ab6fcd7e38e5848dfbcccab1d154
AminaBoby <aminaboby2203@gmail.com>
2022-08-28 14:44:50 +0600

    first comment

---
+++ eighthjs.js
@@ -0,0 +1,33 @@
+// var paraVar = document.querySelector("#paraId");
+// function addStyle(){
+//    paraVar.style.color = "red";
+//    paraVar.style.fontSize = "3re";
+//    paraVar.style.fontWeight =  "bold";
+//    paraVar.style.fontStyle = "italic";
+// }
+
+
+
+// function removeStyle(){
+//     paraVar.style = " ";
+// }
+
+// //better approach
+// var paraVar = document.querySelector("#paraId");
+// function addStyle(){
+//     paraVar.classList = "";
+//    paraVar.classList.add("para-style");
+//     //console.log(paraVar.classList);
+//  }
+ 
+//  function removeStyle(){
+//     paraVar.classList.remove("para-style");
+//  }
+//  function addStyle2(){
+//     paraVar.classList = "";
+//     paraVar.classList.add("para-style-new");
+//   }
+ 
+function nextPage(){
+   location.href="next.html";
+}
